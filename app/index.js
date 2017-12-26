22825aad7cb979032ee314a1d1e03c8cd16e32f1
caixiaoxiao <caixiaoxiao@ngmm365.com>
2017-12-26 10:33:19 +0800

    版本2 部署上线config.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
-require('./main.scss')
+import './main.scss';
 var sub = require('./sub');
 var $ = require('jquery');
 var moment = require('moment');
+
+import './plugin.js'
+
 var app = document.createElement('div');
+let myPromise = Promise.resolve()
 
 app.innerHTML = '<h1>Hello World</h1>'
 app.appendChild(sub())
 document.body.appendChild(app)
 $('body').append('<p>look at me! now is ' + moment().format + '</p>');
+
+myPromise.then((number) => {
+	$('body').append('<p>promise result is' + number + 'now is ' + moment().format + '</p>');
+	//call our jquery plugin
+	$('p').greenify();
+})
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,3 +1,8 @@
+/*
+ *	部署上线 config.js
+ *  删除和开发有关的东西
+ */
+
 var path = require('path')
 var webpack = require('webpack')
 var HtmlwebpackPlugin = require('html-webpack-plugin')
@@ -11,58 +16,41 @@ module.exports = {
 	*	默认会找index.js 也可以确定是哪个文件名
 	*/
 
-	entry: APP_PATH,
+	// entry: APP_PATH,
+	//2版 分离app.js 和 第三方库
+	entry: {
+		app: path.resolve(APP_PATH, 'index.js'),
+		//添加要打包在vendors里面的库
+		vendors: ['jquery', 'moment']
+	},
 
 	//输出的文件名 合并以后的js会命名为bundle.js
 	output: {
 		path: BUILD_PATH,
-		filename: 'bundle.js'
+		filename: '[name].js'
 	},
-
-	/*devServer: {
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		progress: true
-	},*/
-
-	//2版 配置webpack-dev-server代理
-	devServer: {
-		hot: true,
-		inline: true,
-		proxy: {
-			'/api/*': {
-				target: 'http://localhost:5000',
-				secure: false
-			}
-		}
-	},
-
-	devtool: 'source-map', //启用source-map  2版
 
 	module: {
-		loaders: [{
+		rules: [{
 			test: /(\.scss$)/,
 			loader: ["style-loader","css-loader","sass-loader"],
 			include: APP_PATH
 		},{
 			test: /\.(png|jpg)$/,
 			loader: 'url?limit=23'
-		}],
-
-		// preLoaders: [{
-		// 	test: /\.js$/,
-		// 	include: APP_PATH,
-		// 	loader: 'jshint-loader'
-		// }]
-		// rules: [
-		// 	{test: /\.css$/, use:['style-loader', 'css-loader']} 
-		// ]
+		}
+		,{
+			test: /\.js$/,
+			use:[{
+				loader: "babel-loader",
+				options: {
+					presets: ["es2015"]
+				}
+			}],
+			exclude: /node_modules/
+		}
+		],
 	},
-
-	// jshint: {
-	// 	"esnext": true
-	// },
 
 	//添加我们的插件 会自动生成一个html文件
 	plugins: [
@@ -75,6 +63,16 @@ module.exports = {
 			jQuery: "jquery",
 			"window.jQuery": "jquery"
 		}),
+		//使用uglifyJs压缩js代码
+		new webpack.optimize.UglifyJsPlugin({minimize: true}),
+		//将入口文件里面的数组打包成vendor.js
+		
+		// new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+		/* 上面这种写法会报错
+		 * throw new Error(`Deprecation notice: CommonsChunkPlugin now only takes a 
+		 *	single argument. Either an options
+		 */
 
+		new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename:"vendor.bundle.js"})
 	]
 }
